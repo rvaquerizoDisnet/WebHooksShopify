091c32e6240ff5e012eba70d94e61d5356781ea4
rvaquerizoDisnet <raulvaquerizo@disnet.es>
2023-11-30 12:16:30 +0100

    Modalidad, Webhook intercambiable, webservice intercambiable, Seguridad, Log hechos a falta de recibir datos del shopify webhook

--- src/index.js
+++ src/index.js
@@ -1,113 +1,128 @@
-//Utilizo express como middleware para manejar los eventos http
+// FW para manejar solicitudes http
 const express = require('express');
+// Librería para analizar y procesar solicitudes http
 const bodyParser = require('body-parser');
+// Para manejar eventos http
 const axios = require('axios');
+// Para poder recibir desde otro dominio
 const cors = require('cors');
+// Para el cifrado y descifrado de las claves
+const crypto = require('crypto');
+// Para manejar xml
+const xml2js = require('xml2js');
+// Para crear un log
+const winston = require('winston');
+
 const app = express();
-const port = 3000; //Puerto donde escucha
+// Puerto (configurar donde más nos convenga)
+const port = 3000;
 
-app.use(bodyParser.raw({ type: 'application/xml' }));
-app.use(cors()); // Habilita CORS para todas las rutas, sirve para poder recibir desde fuera de nuestra red
-
-//Implementar Funcionalidad,
-//Hacer que compruebe mas de un endpoint, de mas de una empresa o cliente.
-/* Idea de momento:
-
-    Añadir una array con el nombre de clientes que utilizen el webhook de shopify, pero cada vez que se añada un nuevo cliente se tendria que modificar el array
-    ¿Podria sobrecargar el servidor? al hacer el bucle que compruebe todos los clientes
-
-    Otra opcion seria tambien con el mismo array rellenar las urls y que con una funcion que maneje las urls haga las consultas y haga el post
-    atraves de esta funcion y no directamente de la url. Esta opcion seria mas practica, pero tengo que comprobar si es posible
-
-    Tambien hay que pensar un metodo para cambiar la url del post al webservice, ya que en cada cliente tendra un endpoint diferente
-    Para esto tambien existe la posibilidad de hacer otro array con estos endpoints ya que su formato es el siguiente:
-    http://webservice.disnet.es:30000/{11TestPedidos}/
-    En el caso de Print a lot seria http://webservice.disnet.es:30000/15PRTPedidos/
-    (o algo asi no es seguro)
-
-
-    Para que funcione todo esto es necessario que al configurar el webhook el url sea de la siguiente manera:
-    https://disnet.es/{nombrecliente}/webhook/{servicio_requerido(pedidos)}/
-
-    No se si lo alojaremos en disnet.es o en alguno de disnet como del b2b o del erp
-
-    Para comprobar los datos una posible solucion seria crear un archivo xml con la plantilla, algo asi:
-    <!-- schemas/session-info.xsd -->
-    <xs:schema attributeFormDefault="unqualified" elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema">
-    <xs:element name="sessionInfo">
-        <xs:complexType>
-        <xs:sequence>
-            <xs:element type="xs:int" name="customerId"/>
-            <xs:element type="xs:string" name="customerName"/>
-            <xs:element type="xs:string" name="token"/>
-        </xs:sequence>
-        <xs:attribute type="xs:long" name="id"/>
-        </xs:complexType>
-    </xs:element>
-    </xs:schema>
-*/  
-
-
-// Middleware para manejar los webhooks de Shopify - Pedido
-app.post('/nombrecliente/webhook/pedidos', (req, res) => {
-  const data = req.body.toString('utf8');
-
-  if (verificarDatos(data)) {
-    enviarDatosAlWebService(data)
-      .then(response => {
-        console.log('Respuesta del servicio web:', response.data);
-        res.status(200).send('OK');
-      })
-      .catch(error => {
-        console.error('Error al enviar datos al servicio web:', error);
-        res.status(500).send('Error interno del servidor');
-      });
-  } else {
-    console.error('Datos incorrectos');
-    res.status(400).send('Datos incorrectos');
-  }
+// Configuración del logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'webhook-service' },
+  transports: [
+    new winston.transports.File({ filename: '//NAS/L/INFORMATICA/WebServices/log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: '//NAS/L/INFORMATICA/WebService/log/combined.log' }),
+  ],
 });
 
+// Añadir la clave secreta de Shopify para cada cliente
+// Modificar el nombre del cliente por el mismo que añadimos en el array de clientes
+//Datos de ejemplo
+const secretKeys = {
+  cliente1: 'clave_secreta_cliente1',
+  cliente2: 'clave_secreta_cliente2',
+  cliente3: 'clave_secreta_cliente3',
+};
 
+// Array con el nombre de los clientes y sus códigos correspondientes
+//Datos de ejemplo
+const clientes = ['cliente1', 'cliente2', 'cliente3'];
+const codigosClientes = ['11TestPedidos', '12OtroCliente', '13ClienteEjemplo'];
 
-// Middleware para manejar los webhooks de Shopify - Actualización de Pedidos
-app.post('/nombrecliente/webhook/actualizacion_pedidos', (req, res) => {
-  const data = req.body.toString('utf8');
+clientes.forEach((cliente, index) => {
+  const codigoCliente = codigosClientes[index];
+  const rutaPedidos = `/${cliente}/webhook/pedidos`;
+  const rutaActualizacionPedidos = `/${cliente}/webhook/actualizacion_pedidos`;
 
-  if (verificarDatos(data)) {
-    enviarDatosAlWebService(data)
-      .then(response => {
-        console.log('Respuesta del servicio web:', response.data);
-        res.status(200).send('OK');
-      })
-      .catch(error => {
-        console.error('Error al enviar datos al servicio web:', error);
-        res.status(500).send('Error interno del servidor');
-      });
-  } else {
-    console.error('Datos incorrectos');
-    res.status(400).send('Datos incorrectos');
-  }
+  // Middleware para manejar los webhooks de Shopify - Pedido
+  app.post(rutaPedidos, (req, res) => {
+    manejarWebhook(codigoCliente, 'pedidos', req, res);
+  });
+
+  // Middleware para manejar los webhooks de Shopify - Actualización de Pedidos
+  app.post(rutaActualizacionPedidos, (req, res) => {
+    manejarWebhook(codigoCliente, 'actualizacion_pedidos', req, res);
+  });
 });
 
-// Función para verificar los datos sean correctos y añadir algun cambio para que lo sean.
-function verificarDatos(data) {
-  //Falta añadir la funcion para verificarlos
+// Función para verificar la firma de cada cliente y así poder acceder
+function verificarFirmaWebhook(data, hmacHeader, secret) {
+  const hmac = crypto.createHmac('sha256', secret);
+  const contenido = Buffer.from(data, 'utf-8');
+  hmac.update(contenido);
+  const hashCalculado = hmac.digest('base64');
 
+  return hashCalculado === hmacHeader;
+}
 
+async function manejarWebhook(codigoCliente, tipo, req, res) {
+  const data = req.body.toString('utf8');
+  const hmacHeader = req.get('X-Shopify-Hmac-Sha256');
+
+  if (verificarFirmaWebhook(data, hmacHeader, secretKeys[codigoCliente])) {
+    try {
+      const datosJS = await convertirXMLaJS(data);
+      if (verificarDatos(datosJS)) {
+        const response = await enviarDatosAlWebService(datosJS, codigoCliente, tipo);
+        console.log(`Respuesta del servicio web para ${codigoCliente}/${tipo}:`, response.data);
+        res.status(200).send('OK');
+      } else {
+        logger.error(`Datos incorrectos para ${codigoCliente}/${tipo}`);
+        res.status(400).send('Datos incorrectos');
+      }
+    } catch (error) {
+      logger.error(`Error al manejar el webhook para ${codigoCliente}/${tipo}:`, error);
+      res.status(500).send('Error interno del servidor');
+    }
+  } else {
+    logger.error(`Firma incorrecta para ${codigoCliente}/${tipo}`);
+    res.status(401).send('Firma incorrecta');
+  }
+}
+
+function convertirXMLaJS(data) {
+  return new Promise((resolve, reject) => {
+    xml2js.parseString(data, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+function verificarDatos(datos) {
+  // Añadir la función para verificar los datos
   return true;
 }
 
-// Función para enviar datos al servicio web
-function enviarDatosAlWebService(data) {
-  // Añadir la url de nuestro webservice en la constante de abajo
-  const urlWebService = 'http://webservice.disnet.es:30000/';
-  return axios.post(urlWebService, { data });
+// Falta cambiar el nombre del cliente por su código
+function enviarDatosAlWebService(datos, codigoCliente, tipo) {
+  // Añadir la URL de nuestro webservice en la constante de abajo
+  const urlWebService = `https://tu-servicio-web.com/${codigoCliente}${tipo}`;
+  return axios.post(urlWebService, { datos });
 }
 
-//Disparador de evento cuando alguien haga una solicitud se ejecutara el codigo que queramos
-//Middleware escuchando en el puerto que hemos definido arriba (normalmente el 3000)
+//Funcion que envie un correo para avisar que esta habiendo un error y asi poder comprobarlo manualmente
+function enviarCorreo(){
+
+}
+
+// Middleware escuchando en el puerto que hemos definido arriba (normalmente el 3000)
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
-
