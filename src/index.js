0cec7bbbb5e33d9439ac5477a50a3760b3c86494
rvaquerizoDisnet <raulvaquerizo@disnet.es>
2023-12-15 10:50:11 +0100

    falta ssl

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-// index.js
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const apiRouter = require('./api');
 const shopify = require('./shopify');
@@ -19,9 +20,18 @@ const providedUrl = process.env.YOUR_PROVIDED_URL;
 // Inicializar los endpoints con la URL pública 
 shopify.initWebhooks(app, providedUrl);
 
+// Configurar opciones para HTTPS
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/tudominio.com/privkey.pem'), // Ruta al archivo de clave privada generada por Certbot
+  cert: fs.readFileSync('/etc/letsencrypt/live/tudominio.com/fullchain.pem'), // Ruta al archivo de certificado completo generada por Certbot
+}
+
+// Crear servidor HTTPS
+const server = https.createServer(options, app);
+
 // Iniciar el servidor principal
-const server = app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Servidor escuchando en https://localhost:${port}`);
 });
 
 // Manejar eventos de cierre para cerrar correctamente el servidor
