13d634d7c1e4bd08a77d7bf21427090f12f1601a
rvaquerizoDisnet <raulvaquerizo@disnet.es>
2024-02-23 11:39:43 +0100

    aaaaaaa

--- src/gls/glsService.js
+++ src/gls/glsService.js
@@ -11,7 +11,7 @@ const moment = require('moment');
 const csvParser = require('csv-parser');
 
 function consultaAGls() {
-    cron.schedule('40 10 * * *', async () => {
+    cron.schedule('43 10 * * *', async () => {
         // Ejecutar consultas a las 6:00
         console.log('Ejecutando consulta a GLS a las 6:00');
 
