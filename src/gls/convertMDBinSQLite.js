972f3db9f93860a8db6fdca86219803b3b57e407
rvaquerizoDisnet <raulvaquerizo@disnet.es>
2024-02-29 10:01:49 +0100

    aaa

--- src/gls/convertMDBinSQLite.js
+++ src/gls/convertMDBinSQLite.js
@@ -6,7 +6,7 @@ const mdbFilePath = '/home/admin81/shares/GLS/data/expediciones.mdb';
 const csvFilePath = '/home/admin81/shares/GLS/data/expediciones.csv';
 
 function convertTableToCSV() {
-    cron.schedule('01 9 * * *', () => {
+    cron.schedule('03 9 * * *', () => {
         try {
             const exportToCSVCommand = `mdb-export ${mdbFilePath} expediciones > ${csvFilePath}`;
             execSync(exportToCSVCommand);
@@ -19,7 +19,7 @@ function convertTableToCSV() {
 
 
 function deleteCSVFile() {
-    cron.schedule('04 9 * * *', () => {
+    cron.schedule('07 9 * * *', () => {
         const csvFilePath = '/home/admin81/shares/GLS/data/expediciones.csv';
         try {
             fs.unlinkSync(csvFilePath);
